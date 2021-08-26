33eecc3ca3218172ea2c30c9af64c44650532551
Paloma Lozano <palomalogu9@gmail.com>
2021-08-26 10:50:18 +0200

    javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,34 @@
-'use strict';
+/* eslint-disable no-console */
 
-console.log( '>> Ready to create Javascript items :)');
+'use strict';
+//CREANDO VARIABLES PARA LA LISTA DEL MENÚ, estos aparecerán cuando sean llamados
+
+const item1 =   'home';
+const item2 =   'who we are';
+const item3 =   'contact';
+const arrow =   'white arrow';
+
+//LLAMO AL MENÚ DE MI HTML, este es el elemento sobre el que vamos a trabajar
+
+const menu = document.querySelector('.js--dropdown');
+
+//creamos desde aquí el menú (li)
+
+let lista =
+`<li class="menu--home"> ${item1} </li>`;
+
+lista +=
+`<li class="menu--who_we_are"> ${item2} </li>`;
+
+lista +=
+`<li class="menu--contact"> ${item3} </li>`;
+
+lista +=
+`<li class="white--arrow/> ${arrow} </li>`;
+
+menu.innerHTML = lista;
+
+const menuDropdown = document.querySelector( '.js--dropdown');
+
+menuDropdown.classList.add( '.collapsed');
+menuDropdown.classList.remove( '.collapsed');
