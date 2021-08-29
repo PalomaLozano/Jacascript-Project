23ac61763ec4c18de921f0618d03baabdcad3469
Paloma Lozano <palomalogu9@gmail.com>
2021-08-29 18:25:17 +0200

    buttons added

--- src/js/main.js
+++ src/js/main.js
@@ -33,6 +33,9 @@
 //menuDropdown.classList.add( '.collapsed');
 //menuDropdown.classList.remove( '.collapsed');
 
+
+
+//HEADER
 //variables
 const menuCollapsed = document.querySelector('.js--menu');
 const menuHamb = document.querySelector('.js--hamb');
@@ -46,3 +49,96 @@ function handleCollapsable(){
 //Listener
 menuHamb.addEventListener('click', handleCollapsable);
 
+
+//SECOND PART
+
+//VARIABLES
+//variables
+const descriptionCollapsed = document.querySelector('.js--paragraph');
+const descriptionTitle = document.querySelector('.js--title');
+
+//handler
+
+function descriptionCollapsable(){
+  descriptionCollapsed.classList.toggle('collapsed');
+}
+
+//Listener
+descriptionTitle.addEventListener('click', descriptionCollapsable);
+
+
+
+//CONDITIONALS
+//variables
+const descriptionCollapsedTwo = document.querySelector('.js--paragraph_Two');
+const descriptionTitleTwo = document.querySelector('.js--title_Two');
+
+//handler
+
+function descriptionCollapsableTwo(){
+  descriptionCollapsedTwo.classList.toggle('collapsed');
+}
+
+//Listener
+descriptionTitleTwo.addEventListener('click', descriptionCollapsableTwo);
+
+
+
+//FUNCTIONS
+//variables
+const descriptionCollapsedThree = document.querySelector('.js--paragraph_Three');
+const descriptionTitleThree = document.querySelector('.js--title_Three');
+
+//handler
+
+function descriptionCollapsableThree(){
+  descriptionCollapsedThree.classList.toggle('collapsed');
+}
+
+//Listener
+descriptionTitleThree.addEventListener('click', descriptionCollapsableThree);
+
+
+//EVENTS
+//variables
+const descriptionCollapsedFour = document.querySelector('.js--paragraph_Four');
+const descriptionTitleFour = document.querySelector('.js--title_Four');
+
+//handler
+
+function descriptionCollapsableFour(){
+  descriptionCollapsedFour.classList.toggle('collapsed');
+}
+
+//Listener
+descriptionTitleFour.addEventListener('click', descriptionCollapsableFour);
+
+
+//OBJECTS
+//variables
+const descriptionCollapsedFive = document.querySelector('.js--paragraph_Five');
+const descriptionTitleFive = document.querySelector('.js--title_Five');
+
+//handler
+
+function descriptionCollapsableFive(){
+  descriptionCollapsedFive.classList.toggle('collapsed');
+}
+
+//Listener
+descriptionTitleFive.addEventListener('click', descriptionCollapsableFive);
+
+
+//ARRAYS AND LOOPS
+//variables
+const descriptionCollapsedSix = document.querySelector('.js--paragraph_Six');
+const descriptionTitleSix = document.querySelector('.js--title_Six');
+
+//handler
+
+function descriptionCollapsableSix(){
+  descriptionCollapsedSix.classList.toggle('collapsed');
+}
+
+//Listener
+descriptionTitleSix.addEventListener('click', descriptionCollapsableSix);
